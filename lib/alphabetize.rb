b62d452f90bf62cda377531614e877cd3638e06c
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-21 18:43:30 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,8 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
   arr.sort_by do |word|
-  word.split('').map do |letter|
-    ESPERANTO_ALPHABET.index(letter)
+    word.split('').map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
   end
 end
