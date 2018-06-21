6f4af392bac2f7fad375c021a5361d9255191986
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-21 18:43:13 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,16 +1,8 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  # code here
-  esp_array = ESPERANTO_ALPHABET.to_a
-  arr.each do |word|
-    alph_array = word.split("")
-    alph_array.each |char|
-      if alph_array[0] == esp_array[0]
+  arr.sort_by do |word|
+  word.split('').map do |letter|
+    ESPERANTO_ALPHABET.index(letter)
+  end
 end
-
-#arr.sort_by do |word|
- # word_split = word.split("")
-  #word_split.each do |char| 
-   # ESPERANTO_ALPHABET.index(char)
-    # return index in ESPERANTO_ALPHABET corresponding to char
